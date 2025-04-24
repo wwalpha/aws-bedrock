3ae9581cc15a06c71e1a7f8ba60f7660aaacc830
wwalpha <wwalpha@gmail.com>
2025-04-24 08:26:05 +0000

    update

--- backend/auth/src/app.module.ts
+++ backend/auth/src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth/auth.controller';
-import { AuthService } from './auth/auth.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AuthController],
-  providers: [AuthService],
+  imports: [AuthModule],
 })
 export class AppModule {}
