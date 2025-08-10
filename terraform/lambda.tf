49ec5673484498509fdecd48c1f9de6201b96c25
wwalpha <wwalpha@gmail.com>
2025-08-10 14:35:49 +0000

    update

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -4,20 +4,22 @@
 resource "aws_lambda_function" "cognito_post_signup" {
   function_name    = "${local.prefix}_cognito_post_signup"
   role             = aws_iam_role.cognito_post_signup_lambda_role.arn
-  handler          = "lambda_function.handler"
+  handler          = "index.handler"
   runtime          = "nodejs22.x"
   filename         = data.archive_file.default.output_path
   source_code_hash = data.archive_file.default.output_base64sha256
   timeout          = 10
 
   environment {
-    variables = {}
+    variables = {
+      USER_TABLE_NAME = aws_dynamodb_table.user_table.name
+    }
   }
 
-  lifecycle {
-    ignore_changes = [source_code_hash]
-  }
+  # TerraformはdistのZipのみを参照。ビルドはTerraformの外で実行してください。
 }
+
+## ビルドは外部で実行（例: npm run build）。TerraformはZipを配備するだけ。
 
 # ----------------------------------------------------------------------------------------------
 # AWS Lambda Permission - Allow Cognito to Invoke Lambda
