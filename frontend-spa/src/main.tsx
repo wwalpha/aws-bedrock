d7e2f95ef9e640c555b155937011d9a976b2b2de
wwalpha <wwalpha@gmail.com>
2025-08-11 00:30:11 +0000

    update

--- frontend-spa/src/main.tsx
+++ frontend-spa/src/main.tsx
@@ -4,17 +4,23 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import Login from './routes/Login';
 import Root from './routes/Root';
+import Demo from './routes/Demo';
+import { Toaster } from '@/components/ui/toaster';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
-    children: [{ path: '/login', element: <Login /> }],
+    children: [
+      { path: '/login', element: <Login /> },
+      { path: '/demo', element: <Demo /> },
+    ],
   },
 ]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+  <RouterProvider router={router} />
+  <Toaster />
   </StrictMode>
 );
